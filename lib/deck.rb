14e12193a7acdb607c7b360e09887a27bb986aed
Seth Schwartz <sschwartz214@gmail.com>
2018-05-09 16:27:01 -0600

    adds sort method to deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,18 @@ class Deck
     @cards.count
   end
 
+  def sort
+    n = cards.length
+    loop do
+      swapped = false
+      (n-1).times do |i|
+        if cards[i].value.to_i > cards[i + 1].value.to_i
+          cards[i], cards[i + 1] = cards[i + 1], cards[i]
+          swapped = true
+        end
+      end
+      break if swapped == false
+    end
+    cards
+  end
 end
